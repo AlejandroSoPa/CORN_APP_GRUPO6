86df11a843d091e7309e01e2b527d4c12fd0dd35
AlejandroSoPa <alejandrosoldado03@gmail.com>
2023-02-16 16:52:48 +0100

    Initial commit 2

---
+++ app/src/main/java/com/example/corn_app_grupo6/ui/inici/IniciFragment.java
@@ -0,0 +1,38 @@
+package com.example.corn_app_grupo6.ui.inici;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.corn_app_grupo6.R;
+
+public class IniciFragment extends Fragment {
+
+    private IniciViewModel mViewModel;
+
+    public static IniciFragment newInstance() {
+        return new IniciFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inici, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(IniciViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/corn_app_grupo6/ui/inici/IniciViewModel.java
@@ -0,0 +1,7 @@
+package com.example.corn_app_grupo6.ui.inici;
+
+import androidx.lifecycle.ViewModel;
+
+public class IniciViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
